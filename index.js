9536fc8557402540575dbf5005579151ab3b1f38
Mark Wanjau <theafrikanson@gmail.com>
2022-11-27 18:16:36 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// code your solution here
+function saturdayFun(){
+    return `This Saturday, I want to ...!`;
+}
+saturdayFun();
+
+function saturdayFun(activity = "roller-skate"){
+    return `This Saturday, I want to ${activity}!`;
+}
+saturdayFun();
+
+function saturdayFun(activity = "roller-skate"){
+    return `This Saturday, I want to ${activity}!`;
+}
+saturdayFun("bathe my dog");
+
+
+
+
+
+const mondayWork = function(activity = "go to the office"){
+    return `This Monday, I will ${activity}.`
+}
+mondayWork();
+
+
+
+
+
+function wrapAdjective(wrapper="*"){
+    return function(msg="special") {
+        return `You are ${wrapper}${msg}${wrapper}!`
+    }
+}
+console.log(wrapAdjective());
+console.log(wrapAdjective("||"))
